0e458db6e83611009d13064b3d9a912afd7fcb8b
branklth19 <theresa.brankl@edu.fh-joanneum.at>
2022-11-02 16:22:21 +0100

    BMIClassification v1

--- src/BMIClassification.java
+++ src/BMIClassification.java
@@ -1,4 +1,15 @@
 public class BMIClassification {
 
+    public static String classifyBMI(double bmi) {
 
+        // check BMI range
+        if(bmi < 18.5)
+            return "Underweight";
+        else if(bmi < 25)
+            return "Healthy Weight";
+        else if(bmi < 30)
+            return "Overweight";
+        else
+            return "Obesity";
+    }
 }
