bb455c78d35ffbe69a0b02e94912f5512b6ba3fd
branklth19 <theresa.brankl@edu.fh-joanneum.at>
2022-11-02 15:57:16 +0100

    waist hip calculator v1

--- src/WaistHipCalculator.java
+++ src/WaistHipCalculator.java
@@ -1,3 +1,20 @@
 public class WaistHipCalculator {
 
+    double wH_weightInKg;
+    double wH_heightInCm;
+
+    public WaistHipCalculator(double weight, double height) {
+        wH_weightInKg = weight;
+        wH_heightInCm = height;
+
+    }
+
+    public static double wH_Calculation(double wH_weightInKg, double wH_heightInCm) {
+
+        double wH_result =  wH_weightInKg / wH_heightInCm;
+
+        return wH_result;
+
+    }
+
 }
