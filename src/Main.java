3fa43941d129041e0d33d9f8a84c3a627eab22b0
Katharina <katharina.kirchsteiger@edu.fh-joanneum.at>
2022-11-02 17:13:30 +0100

    integrated classification-method in main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
+
+
+    static Classification clas = new Classification();
+
     public static void main(String[] args) {
 
         Scanner myScan = new Scanner(System.in);  // Create a Scanner object
@@ -32,5 +37,14 @@ public class Main {
 
         System.out.println("Your Waste to Hip Ratio is: " + UserWhc);
 
+        System.out.println("Would you like to know your risk for coronary heart disease? Enter Yes or No");
+        String s = myScan.next();
+
+        if (s.equalsIgnoreCase("yes")){
+            System.out.println("Enter your gender. Say true if you are female, false if you are male");
+            boolean gender = myScan.nextBoolean();
+            String myRisk = clas.calculateRisk(gender, UserWhc);
+            System.out.println(myRisk);
+        }
     }
 }
