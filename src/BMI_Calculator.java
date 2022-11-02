45a347791775ad83a610700c959465e63e0288be
lisah <lisa.heiler@edu.fh-joanneum.at>
2022-11-02 15:45:16 +0100

    add method to calc bmi

--- src/BMI_Calculator.java
+++ src/BMI_Calculator.java
@@ -1,5 +1,19 @@
 public class BMI_Calculator {
 
+    int age;
+    double weight;
+    int height;
+    double bmi;
 
+    public BMI_Calculator(int age, double weight, int height) {
+        this.age = age;
+        this.weight = weight;
+        this.height = height;
+    }
 
+    //methode to calculate the bmi
+   public double calculate_bmi (double weight, int height){
+        bmi = weight/(height*height);
+        return bmi;
+   }
 }
